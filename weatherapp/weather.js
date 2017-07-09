ccba256b2ff93f790ae3a32b2dba17562dbdc75c
Joshua Williams <joshuawilliams@Joshuas-iMac.local>
2017-07-09 00:36:18 -0400

    Update #35292

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -36,28 +36,41 @@ function weatherRequest(latitude, longitude, region, city) {
 			document.getElementById("tf").innerHTML = "<span style='color: #888888;'>Temperature:</span> " + tempF + " &deg<span style='color: #5555FF;'>F</span>";
 			
 			// Pick a background image for the current conditions
-			if (wResponse.currently.icon === "clear-day") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/blue_heaven-wallpaper-2048x1152.jpg') no-repeat center center fixed";			
-			} else if (wResponse.currently.icon === "clear-night") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/milky_way_glitters_over_arches_national_park_utah-wallpaper-2048x1152.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "rain") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/rainy_weather-wallpaper-2048x1152.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "snow") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/snowy_mountains_3-wallpaper-2048x1152.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "sleet") {
-				document.body.style.background = "url('http://mediad.publicbroadcasting.net/p/wunc/files/201602/Sleet_on_the_ground.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "wind") {
-				document.body.style.background = "url('https://w-dog.net/wallpapers/9/2/432722453144335/windy-day-landscape-dramatic-sky-tree.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "fog") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/foggy_landscape_bulgaria-wallpaper-2048x1152.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "cloudy") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/cloudy_day_4-wallpaper-2048x1152.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "partly-cloudy-day") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/beautiful_blue_cloudy_sky-wallpaper-2048x1152.jpg') no-repeat center center fixed";
-			} else if (wResponse.currently.icon === "partly-cloudy-night") {
-				document.body.style.background = "url('http://wallpaperswide.com/download/full_moon_night_2-wallpaper-2048x1152.jpg') no-repeat center center fixed";
+			switch (wResponse.currently.icon) {
+				case "clear-day":
+					document.body.className = "clear-day";			
+					break;
+				case "clear-night":
+					document.body.className = "clear-night";			
+					break;
+				case "rain":
+					document.body.className = "rain";			
+					break;
+				case "snow":
+					document.body.className = "snow";			
+					break;
+				case "sleet":
+					document.body.className = "sleet";			
+					break;
+				case "wind":
+					document.body.className = "wind";			
+					break;
+				case "fog":
+					document.body.className = "fog";			
+					break;
+				case "cloudy":
+					document.body.className = "cloudy";			
+					break;
+				case "partly-cloudy-day":
+					document.body.className = "partly-cloudy-day";			
+					break;
+				case "partly-cloudy-night":
+					document.body.className = "partly-cloudy-night";			
+					break;
+				default:
+					document.body.className = "partly-cloudy-day";			
 			}
-
+			
 			// Make sure the default background color is black
 			document.body.style.backgroundColor = "#000000";
 			
