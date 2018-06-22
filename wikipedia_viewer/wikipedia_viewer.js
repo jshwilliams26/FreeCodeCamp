ead48a7d9aaf2b075d2597acfcd60e9a438b61ff
jshwilliams26 <jshwilliams26@gmail.com>
2018-06-22 01:54:01 -0700

    Added character encoding

--- wikipedia_viewer/wikipedia_viewer.js
+++ wikipedia_viewer/wikipedia_viewer.js
@@ -4,7 +4,7 @@ function wikiSearch() {
   document.getElementById('querySubmit').addEventListener('click', function(event) {
     var search = document.getElementById('searchQuery').value;
     var res = "";
-    
+
     $.ajax({
       url: 'https://en.wikipedia.org/w/api.php',
       data: { action: 'query', generator: 'search', gsrsearch: search, prop: 'extracts|allimages', exintro: 1, exlimit: 10, exchars: 200, explaintext: 1, aiprop: "url", format: 'json' },
@@ -18,7 +18,7 @@ function wikiSearch() {
         document.getElementById('result').innerHTML = res;
       }
     });
-    
+
     event.preventDefault();
   });
 }
